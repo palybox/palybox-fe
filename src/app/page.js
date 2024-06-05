9e9dd835e36f0af38baba5ca94f4b2c4166858a5
Brian Felix <fbrian100@gmail.com>
2024-06-05 19:17:43 -0400

    Design updates

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,29 @@ export default function Home() {
       <div>
         <Navigation />
         <section className={`${styles.section} ${styles.heroSection}`}>
+          <div className={styles.heroGrid}>
+            <div className={styles.posOne}>
+              <Image
+                src="/icons/Cannabis Jar.svg"
+                width={250}
+                height={250}
+              ></Image>
+            </div>
+            <div className={styles.posTwo}>
+              <Image
+                src="/icons/White Box.svg"
+                width={250}
+                height={250}
+              ></Image>
+            </div>
+          </div>
           <h5 className={styles.h5}>Conciously curated cannabis</h5>
-          <h3 className={styles.h3}>
-            Reclaim, control over your cannabis experience with our{" "}
-            <span className={styles.highlight}>monthly</span> curated boxes.
-          </h3>
+          <div className={styles.heroText}>
+            <h3 className={styles.h3}>
+              Reclaim, control over your cannabis experience with our{" "}
+              <span className={styles.highlight}>monthly</span> curated boxes.
+            </h3>
+          </div>
           <div className={styles.centeredDiv}>
             <form className={styles.formContainer}>
               <input
@@ -101,6 +119,9 @@ export default function Home() {
             <h2>
               <span className={styles.chunk}>Trusted</span> Partners
             </h2>
+            <div className={styles.crossPos}>
+              <Image src="/icons/Cross.svg" width={200} height={200}></Image>
+            </div>
           </div>
           <div>
             <h3 className={styles.h3}>
@@ -109,57 +130,97 @@ export default function Home() {
               <span className={styles.highlight}>growers</span> in Colorado.
             </h3>
           </div>
+          <div></div>
         </section>
         <section className={`${styles.section} ${styles.aboutSection}`}>
-          <div
-            className={`${styles.subHeadingStripe} ${styles.subHeadingStripeLime}`}
-          >
-            <h2>
-              About the <span className={styles.empha}>Brand</span>
-            </h2>
-          </div>
-          <div className={`${styles.aboutBrand} ${styles.sixtyDiv}`}>
-            <div>
-              <h3>Quality. Inclusivity. Education.</h3>
-              <p>
-                At Paly, we aim to provide an unparalleled cannabis experience
-                by delivering top-tier products directly to your doorstep.
-              </p>
-              <p>
-                <strong>
-                  We envision a future where quality cannabis is available,
-                  accessible, and enjoyed by all.
-                </strong>
-              </p>
+          <div className={styles.aboutGrid}>
+            <div>placeholder for image</div>
+            <div className={styles.aboutBrand}>
+              <div
+                className={`${styles.subHeadingStripePurp} ${styles.paddingRight}`}
+              >
+                <h2>
+                  About the <span className={styles.empha}>Brand</span>
+                </h2>
+              </div>
+              <div>
+                <div className={styles.paddingRight}>
+                  <h3>Quality. Inclusivity. Education.</h3>
+                  <p>
+                    At Paly, we aim to provide an unparalleled cannabis
+                    experience by delivering top-tier products directly to your
+                    doorstep.
+                  </p>
+                  <p>
+                    <strong>
+                      We envision a future where quality cannabis is available,
+                      accessible, and enjoyed by all.
+                    </strong>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div
-            className={`${styles.subHeadingStripe} ${styles.subHeadingStripePurp}`}
-          >
-            <h2>
-              About the <span className={styles.empha}>Founder</span>
-            </h2>
-          </div>
-          <div className={`${styles.aboutFounder} ${styles.sixtyDiv}`}>
-            <div>
-              <h3>Aja Palmino</h3>
-              <p>
-                Aja is a pioneer in the cannabis space with over a decade of
-                experience in the industry.
-              </p>
-              <p>
-                <strong>
-                  She is passionate about incorporating a more empathetic
-                  approach to cannabis so that there is a restored faith in the
-                  industry.
-                </strong>
-              </p>
+            <div className={styles.aboutFounder}>
+              <div
+                className={`${styles.subHeadingStripeLime} ${styles.paddingLeft}`}
+              >
+                <h2>
+                  About the <span className={styles.empha}>Founder</span>
+                </h2>
+              </div>
+              <div>
+                <div className={styles.paddingLeft}>
+                  <h3>Aja Palmino</h3>
+                  <p>
+                    Aja is a pioneer in the cannabis space with over a decade of
+                    experience in the industry.
+                  </p>
+                  <p>
+                    <strong>
+                      She is passionate about incorporating a more empathetic
+                      approach to cannabis so that there is a restored faith in
+                      the industry.
+                    </strong>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.ajaPort}>
+              <Image
+                src="/media/Aja MAsk.png"
+                width={700}
+                height={700}
+                ></Image>
             </div>
           </div>
         </section>
         <section className={`${styles.section} ${styles.kitSection}`}>
+          <div className={styles.greenStarPosOne}>
+            <Image src="/media/7-green.svg" width={700} height={700}></Image>
+          </div>
+          <div className={styles.greenStarPosTwo}>
+            <Image src="/media/7-green.svg" width={700} height={700}></Image>
+          </div>
           <div className={styles.centeredDiv}>
-            <h4>Stay in our loop</h4>
+            <div className={styles.kitHeading}>
+              <div className={styles.fiveStarPos}>
+                <Image
+                  src="/icons/5.svg"
+                  width={150}
+                  height={150}
+                  style={{ objectFit: "contain" }}
+                ></Image>
+              </div>
+              <h4>Stay in our loop</h4>
+              <div className={styles.heartPos}>
+                <Image
+                  src="/icons/Heart.svg"
+                  width={150}
+                  height={150}
+                  style={{ objectFit: "contain" }}
+                ></Image>
+              </div>
+            </div>
           </div>
           <h3 className={styles.h3}>
             We want to get to know{" "}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,31 +7,53 @@ const Footer = () => {
   return (
     <footer>
       <div className={styles.footer}>
-        <div>
-          <h3>Questions? We&apos;d love to hear from you!</h3>
-        </div>
-        <div>
-          <p>
-            Contact us at{" "}
-            <Link href="mailto:hello@palybox.com">hello@palybox.com</Link>
-          </p>
-        </div>
-        <div className={styles.socialWrapper}>
-          <Link href="https://" className={styles.socialIcons}>
-            <Image src="/social/tiktok.svg" width="50" height="50"></Image>
-          </Link>
-          <Link href="https://" className={styles.socialIcons}>
-            <Image src="/social/instagram.svg" width="50" height="50"></Image>
-          </Link>
-          <Link href="https://" className={styles.socialIcons}>
-            <Image src="/social/facebook.svg" width="50" height="50"></Image>
-          </Link>
-          <Link href="https://" className={styles.socialIcons}>
-            <Image src="/social/linkedin.svg" width="50" height="50"></Image>
-          </Link>
-          <Link href="https://" className={styles.socialIcons}>
-            <Image src="/social/youtube.svg" width="50" height="50"></Image>
-          </Link>
+        <div className={styles.footerGrid}>
+          <div className={styles.footerLogo}>
+            <Image src="/media/Logomark1.png" width={800} height={800}></Image>
+          </div>
+          <div className={styles.footerInfo}>
+            <div>
+              <h3>Questions? We&apos;d love to hear from you!</h3>
+            </div>
+            <div>
+              <p>
+                Contact us at{" "}
+                <Link href="mailto:hello@palybox.com">hello@palybox.com</Link>
+              </p>
+            </div>
+            <div className={styles.socialWrapper}>
+              <Link href="https://" className={styles.socialIcons}>
+                <Image src="/social/tiktok.svg" width="50" height="50"></Image>
+              </Link>
+              <Link href="https://" className={styles.socialIcons}>
+                <Image
+                  src="/social/instagram.svg"
+                  width="50"
+                  height="50"
+                ></Image>
+              </Link>
+              <Link href="https://" className={styles.socialIcons}>
+                <Image
+                  src="/social/facebook.svg"
+                  width="50"
+                  height="50"
+                ></Image>
+              </Link>
+              <Link href="https://" className={styles.socialIcons}>
+                <Image
+                  src="/social/linkedin.svg"
+                  width="50"
+                  height="50"
+                ></Image>
+              </Link>
+              <Link href="https://" className={styles.socialIcons}>
+                <Image src="/social/youtube.svg" width="50" height="50"></Image>
+              </Link>
+            </div>
+            <div className={styles.footerHand}>
+              <Image src="/media/Hand.svg" width={500} height={500}></Image>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
